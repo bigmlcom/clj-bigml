6b29eb68c24a46ede2c8fee61e4178ea9795a07f
Adam Ashenfelter <ashenfad@gmail.com>
2012-12-07 21:32:14 -0800

    Update project license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-bigml "0.1.0-SNAPSHOT"
   :description "Clojure access to the BigML.io API"
   :url "https://bigmlcom.github.com/clj-bigml"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.3"]
                  [org.clojure/data.csv "0.1.2"]
