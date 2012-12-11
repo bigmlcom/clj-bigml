f3853749a9af0bfc05e62f8c9cf73f4a0c03bd0e
Adam Ashenfelter <ashenfad@gmail.com>
2012-12-11 14:23:09 -0800

    Project description touchup

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-bigml "0.1.0-SNAPSHOT"
-  :description "Clojure access to the BigML.io API"
+  :description "Clojure bindings for the BigML.io API"
   :url "https://bigmlcom.github.com/clj-bigml"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
