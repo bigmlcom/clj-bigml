cf3236cb18e02657b78b7f78005177dcb3967c0d
Adam Ashenfelter <ashenfad@gmail.com>
2012-12-12 20:59:42 -0800

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-bigml "0.1.0-SNAPSHOT"
+(defproject clj-bigml "1.0.0"
   :description "Clojure bindings for the BigML.io API"
   :url "https://bigmlcom.github.com/clj-bigml"
   :license {:name "Apache License, Version 2.0"
