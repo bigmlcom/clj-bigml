d21304befb2cdd54e3a26df1e4b797830a19875d
jao <jao@bigml.com>
2014-05-05 17:55:26 +0200

    Dependency versions updated

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject bigml/clj-bigml "0.1.0"
+(defproject bigml/clj-bigml "0.2.0-SNAPSHOT"
   :description "Clojure bindings for the BigML.io API"
   :url "https://github.com/bigmlcom/clj-bigml"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "5.0.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.3.1"]
                  [org.clojure/data.csv "0.1.2"]
-                 [clj-http "0.6.0"]
+                 [clj-http "0.9.1"]
                  [commons-validator "1.4.0"]])
