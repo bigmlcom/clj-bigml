e05e0248fdde58f08d8eb5f563217735260c61a8
Sergio De Simone <desimone@bigml.com>
2016-02-12 22:57:06 +0100

    add method to create a sequence of resources to test.utils + refactor

--- test/bigml/test/api/t01.clj
+++ test/bigml/test/api/t01.clj
@@ -10,26 +10,14 @@
                        [model :as model]
                        [prediction :as prediction]
                        [evaluation :as evaluation])
+            (bigml.test.api [utils :as test])
             (clojure [data :as data]))
   (:use clojure.test))
 
-;; this should go into utils
-(defn- do-verb [verb res-type res-uuid & params]
-  (let [resource (api/get-final (apply
-                                (ns-resolve
-                                 (symbol (clojure.string/join
-                                          ["bigml.api." res-type]))
-                                 (symbol verb))
-                                res-uuid params))]
-     (api/get (:resource resource))))
-
-(defn- create [res-type res-uuid & params]
-   (apply do-verb "create" res-type res-uuid params))
-
-(deftest t01
+(deftest ts01
   (api/with-mode :dev
-    (let [src (api/get-final (source/create "test/data/iris.csv.gz"))
-          dst (create "dataset" (:resource src))
-          mdl (create "model" (:resource dst))
-          prd (create "prediction" (:resource mdl) {"petal width" 0.5})]
+    (let [prd (test/create
+               ["source" "dataset" "model" "prediction"]
+               "test/data/iris.csv.gz"
+               {:prediction {"petal width" 0.5}})]
       (is (= "Iris-setosa" (first (vals (:prediction prd))))))))
--- test/bigml/test/api/utils.clj
+++ test/bigml/test/api/utils.clj
@@ -14,3 +14,34 @@
   (let [username (System/getenv "BIGML_USERNAME")
         api-key (System/getenv "BIGML_API_KEY")]
     (api/make-connection username api-key)))
+
+(defn- do-verb
+  "this method is a sort of universal wrappers around individual create
+   functions that reside in specific namespaces. it enables the syntax:
+      (create source file-path options)
+   in place of the more cumbersome
+      (bigml.api.source/create file-path options)"
+  [verb res-type res-uuid & params]
+  (api/get-final (apply
+                  (ns-resolve
+                   (symbol (clojure.string/join
+                            ["bigml.api." res-type]))
+                   (symbol verb))
+                  res-uuid params)))
+
+(defn create
+  "This method creates either a single resource, or a sequence of resources
+   res-type is the resource type(s) to create, either a string or an array
+   res-uuid is the uuid of the resource which is used to create the requested
+         resource or the first of the sequence; it can be a file path
+   params is either a list of option for the single-resource case, or
+       a map from a resource type (represented by a keyword) and
+       another map representing the options to use for that resource type"
+  [res-type res-uuid & params]
+  (if (string? res-type)
+    (apply do-verb "create" res-type res-uuid params)
+    (api/get-final (reduce
+                    #(:resource
+                      (apply create %2 %1 ((keyword %2) (first params))))
+                    res-uuid
+                    res-type))))
