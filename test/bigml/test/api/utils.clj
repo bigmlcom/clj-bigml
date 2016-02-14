ab25b817a0a7ee294ed51ad46172d1f168af6b3e
Sergio De Simone <desimone@bigml.com>
2016-02-14 22:54:42 +0100

    add auto-cleanup to test create method

--- test/bigml/test/api/utils.clj
+++ test/bigml/test/api/utils.clj
@@ -22,26 +22,35 @@
    in place of the more cumbersome
       (bigml.api.source/create file-path options)"
   [verb res-type res-uuid & params]
-  (api/get-final (apply
-                  (ns-resolve
-                   (symbol (clojure.string/join
-                            ["bigml.api." res-type]))
-                   (symbol verb))
-                  res-uuid params)))
+  (apply (ns-resolve
+          (symbol (clojure.string/join ["bigml.api." res-type]))
+          (symbol verb))
+         res-uuid params))
 
 (defn create
-  "This method creates either a single resource, or a sequence of resources
+  "This function creates either a single resource, or a sequence of resources.
+   It returns an array of the UUID of the resource created or an array thereof.
    res-type is the resource type(s) to create, either a string or an array
    res-uuid is the uuid of the resource which is used to create the requested
-         resource or the first of the sequence; it can be a file path
-   params is either a list of option for the single-resource case, or
+         resource or the first one of the sequence; it can be a file path/url
+   params is either a list of options for the single-resource case, or
        a map from a resource type (represented by a keyword) and
        another map representing the options to use for that resource type"
   [res-type res-uuid & params]
   (if (string? res-type)
-    (apply do-verb "create" res-type res-uuid params)
-    (api/get-final (reduce
-                    #(:resource
-                      (apply create %2 %1 ((keyword %2) (first params))))
-                    res-uuid
-                    res-type))))
+    (api/get-final (apply do-verb "create" res-type res-uuid params))
+    (reduce
+     #(conj %1 (:resource
+                (apply create %2 (last %1) ((keyword %2) (first params)))))
+     [res-uuid]
+     res-type)))
+
+(defn create-get-cleanup
+  "This function wraps create so it does a GET of the last resource
+   returned by create and returns it; additionally, it deletes remotely
+   all created resources."
+  [res-type res-uuid & params]
+  (let [rs (apply create res-type res-uuid params)
+        result (api/get-final (last rs))]
+    (doall (pmap api/delete (drop 1 rs)))
+    result))
