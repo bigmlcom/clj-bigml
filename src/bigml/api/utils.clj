a809baf0421828f960d2391af25b69b5e05eca2f
Sergio De Simone <desimone@bigml.com>
2016-04-30 22:38:05 +0200

    refactor based on code review

--- src/bigml/api/utils.clj
+++ src/bigml/api/utils.clj
@@ -3,22 +3,21 @@
 ;; http://www.apache.org/licenses/LICENSE-2.0
 
 (ns bigml.api.utils
-  "Offers functions specific for BigML clusters.
-     https://bigml.com/developers/clusters"
-  (:require [clojure.set :as set])
-  (:require (bigml.api [core :as api])))
+  "Offers generally useful functions for BigML resources."
+  (:require [clojure.set :as set]
+            (bigml.api [core :as api])))
 
 (defn resource-type
   "Returns the resource type of the specified resource.
-   The resource can be specified through:
-    - its full UUID, e.g., 'model/12234567890';
-    - a minimal map-representation, e.g., { :resource '...' };
-    - a full JSON representation."
+  The resource can be specified through:
+  - its full UUID, e.g., 'model/12234567890';
+  - a minimal map-representation, e.g., { :resource '...' };
+  - a full JSON representation."
   [resource]
   (let [resource (or (:resource resource) (str resource))]
     (first (clojure.string/split resource #"/"))))
 
-(defn- if-contains
+(defn- get-keypath
   "Returns the passed map m if it contains the given keypath ks,
   otherwise nil. A keypath is a sequence of keys for recursive lookup."
   [m ks]
@@ -26,36 +25,37 @@
 
 (defn normalized-resource
   "Makes sure the passed resource contains enough information for input
-   normalization (i.e., :input_fields and additionally :restype/field
-   when by-name is true. If it does not, then the resoure is fetched."
+  normalization (i.e., :input_fields and additionally :restype/field
+  when by-name is true. If it does not, then the resoure is fetched."
   [resource by-name]
-  (let [ res-type (resource-type resource)]
-   (or (and (if-contains resource [:input_fields])
-            (or (if-contains resource [(keyword res-type) :fields])
-                (and (not by-name) resource)))
-       (api/get-final resource))))
+  (let [res-type (resource-type resource)]
+    (or (and (map? resource)
+             (contains? resource :input_fields)
+             (or (get-keypath resource [(keyword res-type) :fields])
+                 (and (not by-name) resource)))
+        (api/get-final resource))))
 
 (defn- field-name-mapping
   "Returns a map that associates the passed resource's field names to
   the respective field IDs. E.g.:
-      {'petal width' '000001' 'sepal length '000002' }"
+  {'petal width' '000001' 'sepal length '000002' }"
   [resource]
   (let [res-type (resource-type resource)]
-    (reduce #(assoc %1 (:name (second %2)) (first %2))
+    (reduce (fn [m [k v]] (assoc m (:name v) k))
             {}
-            (seq (:fields ((keyword res-type) resource))))))
+            (:fields ((keyword res-type) resource)))))
 
 (defn normalized-inputs
   "Converts inputs to their map normal form if provided as list,
-   in which case they are associated to input fields in the order
-   the latter appeared during training."
+  in which case they are associated to input fields in the order
+  the latter appeared during training."
   [resource inputs by-name]
   (let [resource (or (normalized-resource resource by-name)
                       (throw (Exception. "Inaccessible/wrong resource")))
         input-fields (:input_fields resource)]
     (if (and (map? inputs) by-name)
       (set/rename-keys inputs (field-name-mapping resource))
-      (apply hash-map (flatten (map list input-fields inputs))))))
+      (zipmap input-fields inputs))))
 
 (defn get-form-params-in
   "Filters parameters that are meant to be sent as HTTP form fields."
